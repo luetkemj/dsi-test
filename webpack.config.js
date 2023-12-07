8221764d5fbd74224077f02c57fd89d3041e3bdb
Mark Luetke <luetkemj@gmail.com>
2023-12-07 05:45:19 -0700

    webpack devserver

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,29 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   entry: {
     index: "./src/index.js",
-    print: "./src/print.js"
+    print: "./src/print.js",
   },
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'DevMode'
-    })
+      title: "DevMode",
+    }),
   ],
-
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
+  },
+  // needed because we have multiple entry points
+  // see here for more details https://webpack.js.org/guides/development
+  optimization: {
+    runtimeChunk: "single",
   },
 };
